dd4360059da64f65698a031488a34798146e8626
Rodrigo Moura <rodrigo.moura@pmi.com>
2023-03-01 15:54:52 +0000

    start-server.sh enhancement

--- sh/start-server.sh
+++ sh/start-server.sh
@@ -1,32 +1,33 @@
 #!/bin/sh
 echo "Available instances:"
 # List instances were Name tag is set to "GPU" and product tag is set to "gpu-streaming"
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value|[0],State.Name]'\
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value|[0],State.Name,PublicDnsName]'\
+    --filter Name=tag:product,Values=gpu-streaming-static --output text
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value|[0],State.Name,PublicDnsName]'\
     --filter Name=tag:product,Values=gpu-streaming --output text
 
 # Enter option by number
 echo "1 - Launch GPU instance"
 echo "2 - Terminate GPU instance"
-echo "3 - Terminate ALL instances"
-echo "4 - Refresh"
-echo "5 - Exit"
+echo "3 - Terminate ALL GPU instances"
+echo "4 - Terminate storagegateway instance"
+echo "9 - Refresh"
+echo "0 - Exit"
 
 read -p "Enter option: " option
 
 case $option in
     1)
         # start storagegateway instance if not running
-        if [ -z "$(aws ec2 describe-instances --query 'Reservations[*].Instances[?Tags[?Key==`Name`].Value|[0]==`storagegateway`].[InstanceId]' --filter 'Name=tag:product,Values=gpu-streaming' 'Name=instance-state-name,Values=running' --output text)" ]; then
+        if [ -z "$(aws ec2 describe-instances --query 'Reservations[*].Instances[?Tags[?Key==`Name`].Value|[0]==`storagegateway`].[InstanceId]' --filter 'Name=tag:product,Values=gpu-streaming-static' 'Name=instance-state-name,Values=running' --output text)" ]; then
             echo "Starting storagegateway instance..."
-            storagegateway_id=$(aws ec2 run-instances --launch-template LaunchTemplateName=storage-gateway --query 'Instances[*].[InstanceId]' --output text)
-            echo $storagegateway_id
+            storagegateway_id=$(aws ec2 describe-instances --query 'Reservations[*].Instances[?Tags[?Key==`Name`].Value|[0]==`storagegateway`].[InstanceId]' --filter 'Name=tag:product,Values=gpu-streaming-static' --output text)
+            aws ec2 start-instances --instance-ids $storagegateway_id --output text
             # wait for storagegateway instance to be running
-            while [ -z "$(aws ec2 describe-instances --query 'Reservations[*].Instances[?Tags[?Key==`Name`].Value|[0]==`storagegateway`].[InstanceId]' --filter 'Name=tag:product,Values=gpu-streaming' 'Name=instance-state-name,Values=running' --output text)" ]; do
+            while [ -z "$(aws ec2 describe-instances --query 'Reservations[*].Instances[?Tags[?Key==`Name`].Value|[0]==`storagegateway`].[InstanceId]' --filter 'Name=tag:product,Values=gpu-streaming-static' 'Name=instance-state-name,Values=running' --output text)" ]; do
                 echo "Waiting for storagegateway instance to be running..."
                 sleep 5
             done
-            # attach cache ebs volume to storagegateway instance
-            aws ec2 attach-volume --volume-id vol-020cdef7aaaaa4743 --device /dev/sdb --instance-id $storagegateway_id --region eu-west-1
         fi
 
         # Launch GPU instance
@@ -45,7 +46,7 @@ case $option in
         echo $gpu_id
 
         # wait for instance with name in instance_name to be running
-        while [ -z "$(aws ec2 describe-instances --instance-ids $gpu_id)" ]; do
+        while [ "$(aws ec2 describe-instances --instance-ids $gpu_id --query Reservations[*].Instances[*].[State.Name] --output text)" != "running" ]; do
             echo "Waiting for instance to be running..."
             sleep 5
         done
@@ -60,14 +61,19 @@ case $option in
         ;;
     3)
         # Terminate all instances
-        echo "Terminating all instances..."
+        echo "Terminating all GPU instances..."
         aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filter Name=tag:product,Values=gpu-streaming --output text)
         ;;
     4)
+        # Stop storagegateway instance
+        echo "Stopping storagegateway instance..."
+        aws ec2 stop-instances --instance-ids $(aws ec2 describe-instances --query 'Reservations[*].Instances[?Tags[?Key==`Name`].Value|[0]==`storagegateway`].[InstanceId]' --filter 'Name=tag:product,Values=gpu-streaming-static' --output text)
+        ;;
+    9)
         # Refresh
         echo "Refreshing..."
         ;;
-    5)
+    0)
         # Exit
         echo "Exiting..."
         exit 0
